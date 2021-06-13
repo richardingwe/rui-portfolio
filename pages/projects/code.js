221f330ae6ef76a9bebf6e928fe225be7ce60561
richardingwe <richardingwe5@gmail.com>
2021-06-13 17:16:31 +0100

    added Layout to code page

---
+++ pages/projects/code.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@/components/Layout';
+
+const code = () => {
+    return (
+        <Layout title='Rui - Code Projects'>
+        </Layout>
+    );
+};
+
+export default code;
