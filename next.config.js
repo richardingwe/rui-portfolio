a7e68fc6e9b4f621d300dcb56e4ce179a8752300
richardingwe <richardingwe5@gmail.com>
2021-06-15 19:57:47 +0100

    made the app a pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-module.exports = {
+const path = require("path");
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+
+module.exports = withPWA({
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        runtimeCaching,
+    },
     images: {
         domains: ['cdn.sanity.io']
     },
@@ -6,4 +17,9 @@ module.exports = {
         PROJECT_ID: 'y0xdnwwh',
     },
     target: 'serverless',
-}
+    // This is not required to make it into a PWA, but is a nice way to clean up your imports
+    webpack: (config) => {
+        config.resolve.modules.push(path.resolve("./"));
+        return config;
+    },
+});
---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    // static async getInitialProps(ctx) {
+    //     const initialProps = await Document.getInitialProps(ctx);
+    //     return { ...initialProps };
+    // }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta name="theme-color" content="#1f2937" />
+                    <link rel="apple-touch-icon" href="/logo192.png" />
+                    <link rel="manifest" href="/manifest.json" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
