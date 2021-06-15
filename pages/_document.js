5d463eee517bf294159a68c772cd6a21f2d59c5e
richardingwe <richardingwe5@gmail.com>
2021-06-15 23:07:37 +0100

    added more SEO tags

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,19 @@ class MyDocument extends Document {
                     <title>Rui - Software Developer & Brand Identity Designer</title>
                     <meta name="author" content="Richard Ingwe" />
                     <meta name="Rui Portfolio" content="Rui Portfolio" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:site_name" content='Richard Ingwe Portfolio' />
+                    <meta property="og:description" content='Richard Unimke Ingwe Creates Beautiful Experiences, That Could Keep Potential Customers Glued & Loyal To Your Businesses, Brands & Websites.' />
+                    <meta property="og:image" content='/logo512.png' />
+                    <meta property="og:image:alt" content='Rui logo' />
+                    <meta property="og:locale" content="en_US" />
+                    <meta name="fb:app_id" content="richard.ingwejr"></meta>
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:site" content="@richardingwe"></meta>
+                    <meta name="twitter:creator" content="@richardingwe"></meta>
+                    <meta name="twitter:description" content='Richard Unimke Ingwe Creates Beautiful Experiences, That Could Keep Potential Customers Glued & Loyal To Your Businesses, Brands & Websites.' />
+                    <meta name="twitter:image" content='/logo512.png' />
+                    <meta name="twitter:image:alt" content='Rui logo' />
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></link>
                 </Head>
                 <body>
