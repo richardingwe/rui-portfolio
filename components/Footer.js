b7120b150ba8fe037df8676c952c8decf76ecd0a
richardingwe <richardingwe5@gmail.com>
2021-06-15 09:53:45 +0100

    made the AOS fade in effect not to work on the home page

--- components/Footer.js
+++ components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
             display: `${location.pathname === '/resume' ? 'none' : 'flex'
                 }`,
         }}>
-            <div data-aos="fade-in" data-aos-delay="200" data-aos-offset="50" >
+            <div data-aos={location.pathname === '/' ? '' : "fade-in"} data-aos-delay="200" data-aos-offset="50" >
                 <span>© 2021 Rui Creative. All rights reserved. </span>
                 {/* <br /> */}
                 <a href="https://github.com/richardingwe" target="_blank" rel="nofollow noopener noreferrer" className="">
