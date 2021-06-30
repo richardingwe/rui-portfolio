2a6c50410618cdedbb6035e1b219aa0c116b1d8a
richardingwe <richardingwe5@gmail.com>
2021-06-30 06:38:51 +0100

    updated the footer to dynamically show year

--- components/Footer.js
+++ components/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
                 }`,
         }}>
             <div data-aos={location.pathname === '/' ? '' : "fade-in"} data-aos-delay="200" data-aos-offset="50" >
-                <span>© 2021 Rui Creative. All rights reserved. </span>
+                <span>© {new Date().getFullYear()} Rui Creative. All rights reserved. </span>
                 {/* <br /> */}
                 <a href="https://github.com/richardingwe" target="_blank" rel="nofollow noopener noreferrer" className="">
                     Designed && Coded By Rui.
