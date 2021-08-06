091c8475a6a7b791b60b60f78bbbb97900a7fe29
richardingwe <richardingwe5@gmail.com>
2021-08-06 05:49:41 +0100

    added see more button to code projects

---
+++ pages/projects/code/[slug].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CodeProjectDetails = () => {
+    return (
+        <div>
+            <h1>Code Project Details</h1>
+        </div>
+    );
+};
+
+export default CodeProjectDetails;
