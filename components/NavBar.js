2ae0bf06ffb866b615395197a87ccc2ce2b13b3b
richardingwe <richardingwe5@gmail.com>
2021-08-10 16:59:25 +0100

    .

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
