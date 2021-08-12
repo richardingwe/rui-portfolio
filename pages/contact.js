66d8ee79a00c2b784b3d382a29ecaec5550f2eed
richardingwe <richardingwe5@gmail.com>
2021-08-12 15:46:49 +0100

    added the email functionality

--- pages/contact.js
+++ pages/contact.js
@@ -9,12 +9,11 @@ import Layout from '@/components/Layout';
 import "animate.css";
 import { useForm, ValidationError } from '@formspree/react';
 import { useThemeContext } from '../context/state';
+import Success from '@/components/Success';
 
 const Post = () => {
 
     const [state, handleSubmit] = useForm("xknkqwpq");
-    const [btnMsg, setBtnMsg] = useState("Send Message");
-    const [sent, setSent] = useState(false);
 
     useEffect(() => {
         AOS.init({
@@ -23,6 +22,9 @@ const Post = () => {
         AOS.refresh();
     }, []);
 
+    if (state.succeeded) {
+        return <Success />;
+    }
 
     const location = useRouter();
     const { theme } = useThemeContext();
@@ -145,7 +147,7 @@ const Post = () => {
                                                             <path fill="currentColor" d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z">
                                                             </path>
                                                         </svg>
-                                                    </span>{btnMsg}</button>
+                                                    </span>{state.submitting ? 'Sending Message' : 'Send Message'}</button>
                                             </div>
                                         </div>
                                     </form>
